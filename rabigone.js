7cd20710cb5d62eb34767b52ab35c6d6b68826b1
Stephen Kraushaar <skraushaar@gmail.com>
2015-01-28 03:54:26 -0600

    Update rabigone.js
    Add off. Meh.

--- rabigone.js
+++ rabigone.js
@@ -1,21 +1,34 @@
-function rabigone() {
-	this.posts = $("a.p-nickname[href^='/@unixrab']");
-	this.replies = this.posts.closest(".reply");
-	this.comments = this.posts.closest(".comment");
-	this.mentions = $("*:contains('unixrab'):not(:has(:contains('unixrab')))"),
-	this.hide = function() {
-		this.replies.hide();
-		this.comments.hide();
+(function() {
+	function rabigone() {
+		this.posts = $("a.p-nickname[href^='/@unixrab']");
+		this.replies = this.posts.closest(".reply");
+		this.comments = this.posts.closest(".comment");
+		this.mentions = $("*:contains('unixrab'):not(:has(:contains('unixrab')))"),
+		this.hide = function() {
+			this.replies.hide();
+			this.comments.hide();
+		};
+		this.show = function() {
+			this.replies.show();
+			this.comments.show();
+		};
+		this.goat = function(mode) {
+			if(!mode) {
+				this.mentions.map(function(){ this.innerText = this.innerText.replace("unixrab","g04t"); });
+			} else {
+				this.mentions.map(function(){ this.innerText = this.innerText.replace("g04t","unixrab"); });
+			}
+		};
+		this.all = function() {
+			this.hide();
+			this.goat();
+		}
+		this.off = function() {
+			this.show();
+			this.goat(true);
+		}
 	};
-	this.show = function() {
-		this.replies.show();
-		this.comments.show();
-	};
-	this.goat = function() {
-		this.mentions.map(function(){ this.innerText = this.innerText.replace("unixrab","goat"); });
-	};
-	this.all = function() {
-		this.hide();
-		this.goat();
-	}
-};
+	
+	window.rabigone = new rabigone();
+	rabigone.all();
+})();
